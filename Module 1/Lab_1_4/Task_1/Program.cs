64cee7397710123019b54819c0e3a4a53d818279
NoxCodeGH <mysocialnet@yandex.ru>
2021-11-01 19:41:33 +0300

    Модуль 1: Лабораторная 4: Задача 1, 2

---
+++ Module 1/Lab_1_4/Task_1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            // Для отображения японских символов в консоли используется шрифт MS Gothic
+
+            string textRu = "Привет мир!";
+            Console.WriteLine($"Исходный текст En: {textRu}");            
+
+            string textEn = "Hello World!";
+            Console.WriteLine($"Исходный текст En: {textEn}");
+
+            string textDe = "Hallo Welt!";
+            Console.WriteLine($"Исходный текст De: {textDe}");
+
+            string textJa = "こんにちは世界";
+            Console.WriteLine($"Исходный текст Ja: {textJa}");            
+
+            Console.WriteLine();
+
+            byte[] bytesRu = Encoding.UTF8.GetBytes(textRu);
+            Console.WriteLine($"Сериализация текста Ru: {BitConverter.ToString(bytesRu)}");
+
+            byte[] bytesEn = Encoding.UTF8.GetBytes(textEn);
+            Console.WriteLine($"Сериализация текста En: {BitConverter.ToString(bytesEn)}");
+
+            byte[] bytesDe = Encoding.UTF8.GetBytes(textDe);
+            Console.WriteLine($"Сериализация текста De: {BitConverter.ToString(bytesDe)}");
+
+            byte[] bytesJa = Encoding.UTF8.GetBytes(textJa);
+            Console.WriteLine($"Сериализация текста Ja: {BitConverter.ToString(bytesJa)}");
+
+            Console.WriteLine();
+
+            string resultRu = Encoding.UTF8.GetString(bytesRu);
+            Console.WriteLine($"Десериализация массива байт Ru: {resultRu}");
+
+            string resultEn = Encoding.UTF8.GetString(bytesEn);
+            Console.WriteLine($"Десериализация массива байт En: {resultEn}");
+
+            string resultDe = Encoding.UTF8.GetString(bytesDe);
+            Console.WriteLine($"Десериализация массива байт De: {resultDe}");
+
+            string resultJa = Encoding.UTF8.GetString(bytesJa);
+            Console.WriteLine($"Десериализация массива байт Ja: {resultJa}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module 1/Lab_1_4/Task_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using System.Reflection;
+
+namespace Task_2
+{
+    class Program
+    {
+        static ExceptionDispatchInfo exceptionDispatchInfo;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 1;
+                int b = 0;
+                Console.WriteLine($"Выражение: result = {a}/{b}");
+
+                int result = a / b;
+                Console.WriteLine($"result = {result}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                exceptionDispatchInfo = ExceptionDispatchInfo.Capture(ex);
+
+                Console.WriteLine($"Регистрация исключения в методе: {MethodBase.GetCurrentMethod().Name}");
+                Console.WriteLine($"Исключение: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine();
+                HandleException();
+
+                Console.ReadKey();
+            }                                   
+        }
+
+        static void HandleException()
+        {
+            try
+            {
+                if (exceptionDispatchInfo != null)
+                    exceptionDispatchInfo.Throw();
+            }
+            catch
+            {
+                Console.WriteLine($"Вызов исключения в методе: {MethodBase.GetCurrentMethod().Name}");
+                Console.WriteLine($"Исключение: {exceptionDispatchInfo.SourceException.Message}");
+            }
+        }
+    }
+}
